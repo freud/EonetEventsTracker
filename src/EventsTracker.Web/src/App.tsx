e1d3720f1f10288f5f4e10c2ea9c6e59aa62c4b6
Paul Sroczynski <p.sroczynski@gmail.com>
2022-11-08 23:02:41 +0100

    Adjust logo and title

--- src/EventsTracker.Web/src/App.tsx
+++ src/EventsTracker.Web/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
-import MapIcon from '@mui/icons-material/Map';
+import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
 import EventsGrid from "./features/events/EventsGrid";
+import logo from './logo.svg'
 
 function App() {
     return (<>
-        <AppBar position="static" sx={{ marginBottom: 3 }}>
+        <AppBar position="static" sx={{ marginBottom: 3, padding: 1 }}>
             <Container>
                 <Toolbar>
-                    <MapIcon sx={{ margin: 1, fontSize: 40 }} />
+                    <Box component="img" src={logo} sx={{ margin: 0, height: 50}} />
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         NASA Earth Observatory Natural Events Tracker
                     </Typography>
