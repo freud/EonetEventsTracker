95e5e16074533498a65c93fccc8f57956ba4c2a9
Paul Sroczynski <p.sroczynski@gmail.com>
2022-11-09 08:42:43 +0100

    Handle clearing selected category

--- src/EventsTracker.Web/src/features/events/CategoriesSelect.tsx
+++ src/EventsTracker.Web/src/features/events/CategoriesSelect.tsx
@@ -1,16 +1,18 @@
-import { FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import { FormControl, IconButton, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import * as React from "react";
 import { useQuery } from "react-query";
 import { Category } from "./EventsGrid";
 import { useState } from "react";
+import ClearIcon from '@mui/icons-material/Clear';
 
 interface CategoriesSelectProps {
-    onCategoryChanged: (category: Category) => void
+    onCategoryChanged: (category: Category | undefined) => void
     category: Category | undefined
 }
 
 export default function CategoriesSelect(props: CategoriesSelectProps) {
-    const [categoryId, setCategoryId] = useState<string | undefined>(props.category ? props.category.id : undefined);
+    const [categoryId, setCategoryId] = useState<string | undefined>(
+        props.category ? props.category.id : '');
     const { isLoading, isFetching, data } = useQuery(
         {
             refetchInterval: false,
@@ -30,18 +32,22 @@ export default function CategoriesSelect(props: CategoriesSelectProps) {
     )
 
     const isInitializing = isLoading || isFetching;
-
+    const clear = () => {
+        setCategoryId('')
+        props.onCategoryChanged(undefined)
+    }
+    const onChange = (e: SelectChangeEvent) => {
+        setCategoryId(e.target.value)
+        const selectedCategory = data?.find(category => category.id === e.target.value);
+        props.onCategoryChanged(selectedCategory)
+    }
     return (<>
         <FormControl variant="filled" fullWidth={true}>
             <InputLabel>Categories</InputLabel>
-            <Select value={categoryId} onChange={(e) => {
-                setCategoryId(e.target.value as string)
-                const selectedCategory = data?.find(category => category.id === e.target.value);
-                if (selectedCategory) {
-                    props.onCategoryChanged(selectedCategory)
-                }
-            }}>
-                {!isInitializing && data && data.map(category => <MenuItem key={category.id} value={category.id}>{category.title}</MenuItem>)}
+            <Select value={categoryId} displayEmpty IconComponent={() => null} onChange={onChange}
+                    endAdornment={<IconButton onClick={clear}><ClearIcon /></IconButton>}>
+                {!isInitializing && data &&
+                    data.map(category => <MenuItem key={category.id} value={category.id}>{category.title}</MenuItem>)}
             </Select>
         </FormControl>
     </>);
